84c31b48c7278f3107d9b203b07ce0dae1bd803d
Dylan Wolff <dylanwolff@gmail.com>
2015-01-28 18:34:35 +1100

    Install figaro

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass', '~> 3.3.3'
 gem "paperclip", "~> 4.2"
 gem "paperclip-dropbox", ">= 1.1.7"
+gem "figaro"
 
 group :production do
   gem 'pg'
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,3 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "no_image.png", :storage => :dropbox,
-                    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+  has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "no_image.png"
 end
