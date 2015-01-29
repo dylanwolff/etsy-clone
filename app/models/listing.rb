0d244c0d9a3e382ca0133b47cd5aca7a74ecba68
Dylan Wolff <dylanwolff@gmail.com>
2015-01-29 11:41:31 +1100

    Add conditional links

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,8 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "no_image.png",
-                    :storage => :dropbox,
-                    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+  has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "no_image.png"
+                    # Disable Dropbox storage of images for development
+                    # , :storage => :dropbox,
+                    # :dropbox_credentials => Rails.root.join("config/dropbox.yml")
 
   validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
 
